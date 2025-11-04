85a80305f2dc15054f7c37f38df97ae91794ef92
Micheliliuv87 <micheliliuv87@gmail.com>
2025-11-04 07:56:38 +0000

    Deploying to gh-pages from @ Micheliliuv87/micheliliuv87.github.io@ae0b7babd91037221ec4636aedbcf8610fa4e839 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -28,7 +28,7 @@ function determineGiscusTheme() {
     "data-emit-metadata": "0",
     "data-input-position": "bottom",
     "data-theme": giscusTheme,
-    "data-lang": "en, cn",
+    "data-lang": "en",
     crossorigin: "anonymous",
     async: true,
   };
